9e23186b55fe66244c90f0aaf1f0e40181062cbd
ArseniKorbush <122732165+ArseniKorbush@users.noreply.github.com>
2023-11-28 23:54:58 +0300

    Time: 56 ms (90.40%), Space: 44 MB (89.32%) - LeetHub

---
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.js
@@ -0,0 +1,24 @@
+function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val);
+    this.left = (left === undefined ? null : left);
+    this.right = (right === undefined ? null : right);
+}
+
+var sortedArrayToBST = function(nums) {
+    const buildBST = (left, right) => {
+        if (left > right) {
+            return null;
+        }
+
+        const mid = Math.floor((left + right) / 2);
+        const root = new TreeNode(nums[mid]);
+
+        root.left = buildBST(left, mid - 1);
+        root.right = buildBST(mid + 1, right);
+
+        return root;
+    };
+
+    return buildBST(0, nums.length - 1);
+};
+// Developed by SENYA
